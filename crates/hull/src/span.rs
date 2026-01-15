f5e4c2566876176fe2f3d18e13f0974fe03cef4a
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2026-01-15 18:38:17 +0900

    Implement span resolution

--- crates/hull/src/span.rs
+++ crates/hull/src/span.rs
@@ -1,10 +1,10 @@
 use std::ops::Add;
 
-use crate::{diag::Offset, input::SourceFile};
-
 use crate::{
     Db,
     ast::{function::FuncBody, item::Item},
+    diag::{AbsoluteSpan, Offset},
+    input::SourceFile,
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, salsa::Update)]
@@ -19,6 +19,36 @@ pub struct Span<'db> {
     anchor: Anchor<'db>,
     begin: Offset,
     end: Offset,
+}
+
+impl<'db> Span<'db> {
+    pub fn source_file(self, db: &'db dyn Db) -> SourceFile {
+        match self.anchor {
+            Anchor::Root(file) => file,
+            Anchor::Item(item) => item.span(db).source_file(db),
+            Anchor::FuncBody(body) => body.span(db).source_file(db),
+        }
+    }
+
+    pub fn resolve_to_absolute(self, db: &'db dyn Db) -> AbsoluteSpan {
+        let add_offset = |base: Offset, rel: Offset| Offset::new(base.as_u32() + rel.as_u32());
+
+        match self.anchor {
+            Anchor::Root(file) => AbsoluteSpan::new(file, self.begin, self.end),
+            Anchor::Item(item) => {
+                let base = item.span(db).resolve_to_absolute(db);
+                let start = add_offset(base.start(), self.begin);
+                let end = add_offset(base.start(), self.end);
+                AbsoluteSpan::new(base.file(), start, end)
+            }
+            Anchor::FuncBody(body) => {
+                let base = body.span(db).resolve_to_absolute(db);
+                let start = add_offset(base.start(), self.begin);
+                let end = add_offset(base.start(), self.end);
+                AbsoluteSpan::new(base.file(), start, end)
+            }
+        }
+    }
 }
 
 impl<'db> Add for Span<'db> {
